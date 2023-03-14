b4bd4aa28e70999a7c3f2cda762820912d215e27
Cristian Tahay Batz <tahayk3@gmail.com>
2023-03-14 12:47:11 -0600

    Contador finalizado con hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import './App.css';
+import logo from './imagenes/logo.png';
+import Boton from './componentes/boton';
+import Contador from './componentes/contador';
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setnumClics] = useState(0);
+
+  const manejarClic = () => {
+    console.log('Clic');
+    setnumClics(numClics + 1);
+  };
+  const reiniciarContador = () =>{
+    console.log('Reiniciando');
+    setnumClics(0);
+  };
+
   return (
     <div className="App">
-      <div className=''>
-
+      <div className='logo-contenedor'>
+        <img 
+          className='logo'
+          src={logo}
+          alt='Logo del contador'
+        />  
+      </div>
+      <div className='contenedor-principal'>
+        <Contador
+        numClics={numClics}
+        />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic}
+        />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
       </div>
     </div>
   );
